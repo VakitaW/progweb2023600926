2531d878c923aba4350e97d8deb80121758ee54d
VakitaW <adriftvaquits101@gmail.com>
2025-03-10 17:33:33 -0600

    priemra version

---
+++ Fibnacci.php
@@ -0,0 +1,19 @@
+<html>
+  <head><title>Fibonacci</head></title>
+    <body>
+      <?php
+        $Numero1 = 0;
+        $Numero2 = 1;
+
+        echo $Numero1 .'<br>';
+        for($i0; $i<2048: $i++);
+        $K = $NUmero1;
+
+        $Numero1 = $Numero2;
+
+        $Numero2 = $K + $Numero1;
+
+        echo $Numero1 . '<br>';
+        ?> 
+    </body>
+</html>
